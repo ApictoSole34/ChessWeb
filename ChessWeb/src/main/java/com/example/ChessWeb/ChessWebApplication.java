73ab62da5a7dfe8e64134be7dbc0a9eded6f7eb4
Bartosz Mazur <mazurbartosz99@gmail.com>
2023-08-23 10:49:33 +0200

    Initial commit + spring

---
+++ ChessWeb/src/main/java/com/example/ChessWeb/ChessWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChessWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessWebApplication.class, args);
+	}
+
+}
---
+++ ChessWeb/src/test/java/com/example/ChessWeb/ChessWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChessWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
